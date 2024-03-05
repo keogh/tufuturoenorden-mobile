ad69b054e85fb179ed1b6f38ec99bafc1ea1d8a3
Isaac Zepeda <417939+keogh@users.noreply.github.com>
2024-03-05 06:59:51 -0700

    Add AI message avatar

--- src/domain/Message/MessageItem.tsx
+++ src/domain/Message/MessageItem.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { View, Text, StyleSheet, StyleProp, ViewStyle } from 'react-native';
 import type { MessageItemType } from './types';
-import { HUMAN_SENDER } from './constants';
+import { AI_SENDER, HUMAN_SENDER } from './constants';
 
 type Props = {
   message: MessageItemType;
@@ -16,8 +16,19 @@ const MessageItem = ({ message }: Props) => {
     viewContentStyles = messagesStyles.messageItemSeparator;
   }
 
+  const isHuman = message.sender === HUMAN_SENDER;
+  const containerStyles: StyleProp<ViewStyle> = [styles.messageItemContainer];
+  if (isHuman) {
+    containerStyles.push(styles.messageItemContainerHuman);
+  }
+
   return (
-    <View style={styles.messageItemContainer}>
+    <View style={containerStyles}>
+      {message.sender === AI_SENDER && (
+        <View style={styles.messageItemAvatarContainer}>
+          <Text style={styles.messageItemAvatar}>🤖</Text>
+        </View>
+      )}
       <View style={viewContentStyles}>
         <Text
           style={
@@ -35,10 +46,29 @@ const MessageItem = ({ message }: Props) => {
 const styles = StyleSheet.create({
   messageItemContainer: {
     flex: 1,
+    justifyContent: 'flex-start',
+    flexDirection: 'row',
+    gap: 5,
+    marginVertical: 5,
+    paddingLeft: 10,
+    paddingRight: 10,
+  },
+  messageItemContainerHuman: {
+    justifyContent: 'flex-end',
+  },
+  messageItemAvatarContainer: {
+    alignSelf: 'flex-end',
+    width: 'auto',
+    height: 'auto',
+    backgroundColor: '#666666', // TODO: Theme color
+    borderRadius: 32,
+    display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
-    flexDirection: 'column',
-    marginVertical: 5,
+    padding: 8,
+  },
+  messageItemAvatar: {
+    fontSize: 28,
   },
   sender: {
     width: 'auto',
@@ -71,15 +101,12 @@ const baseMessagesStyles = StyleSheet.create({
   messageItem: {
     backgroundColor: '#424242', // TODO: Theme color
     color: '#FFF', // TODO: Theme color
-    alignSelf: 'flex-start',
-    maxWidth: '85%',
+    maxWidth: '75%',
     padding: 16,
     borderRadius: 24,
     borderBottomLeftRadius: 0,
-    marginLeft: 5,
     display: 'flex',
     flexDirection: 'column',
-    gap: 5,
   },
 });
 
@@ -88,9 +115,9 @@ const messagesStyles = StyleSheet.create({
     ...baseMessagesStyles.messageItem,
     backgroundColor: '#5a5a5a', // TODO: Theme color
     alignSelf: 'flex-end',
-    marginRight: 5,
     borderBottomRightRadius: 0,
     borderBottomLeftRadius: 24,
+    maxWidth: '85%',
   },
   messageItemSeparator: {
     ...baseMessagesStyles.messageItem,
